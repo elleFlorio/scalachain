e873215b0bbfa7c6678cf2e4f3ab4714dd260a4b
Luca Florio <elle.florio@gmail.com>
2018-08-24 19:28:12 +0200

    fix pow test
    pow test has been fixed to take into account the new validity
    condition for proofs.

---
+++ src/test/scala/proof/ProofOfWorkTest.scala
@@ -0,0 +1,17 @@
+package proof
+
+import org.scalatest.{FlatSpecLike, Matchers}
+
+class ProofOfWorkTest extends Matchers
+  with FlatSpecLike {
+
+  "Validation of proof" should "correctly validate proofs" in {
+    val lastHash = "1"
+    val correctProof = 7178
+    val wrongProof = 0
+
+    ProofOfWork.validProof(lastHash, correctProof) should be (true)
+
+    ProofOfWork.validProof(lastHash, wrongProof) should be (false)
+  }
+}
